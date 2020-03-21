eadf485cdf496a5715982ae53a28fe96fb2334a5
Ricardo Ferreira <ricardo199221@gmail.com>
2020-03-21 16:13:05 +0000

    Alfa version

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-const player = (name, score) => {
-    return {name, score, totalScore}
-}
+//Initial Declarations
 activePlayer = 1;
+let messageBoard = document.getElementById("messageBoard");
+
+const player = (name) => {
+    let score = 0;
+    return {name, score}
+}
+
+//Buttons Event Listener
+let newGame = document.getElementById("newGame");
+let playerOne;
+let playerTwo;
+let onHold = true;
+
+newGame.addEventListener("click", function(){
+    p1name = document.getElementById("input1").value;
+    playerOne = player(p1name);
+    p2name = document.getElementById("input2").value;
+    playerTwo = player(p2name);
+    renderPlayerNames();
+    onHold = false;
+    gameBoard.clearBoard();
+    playerOne.score = 0;
+    playerTwo.score = 0;
+    gameBoard.updateScoreBoard();
+    if(activePlayer === 1){
+        messageBoard.textContent = playerOne.name + ", your turn!"
+    }else{
+        messageBoard.textContent = playerTwo.name + ", your turn!" 
+    }
+});
+
+const renderPlayerNames = () => {
+    let p1NameText = document.getElementById("p1NameText");
+    let p2NameText = document.getElementById("p2NameText");
+    let scoreContainer = document.getElementById("scoreContainer");
+    p1NameText.textContent = playerOne.name + ":  ";
+    p2NameText.textContent = playerTwo.name + ":  ";
+    scoreContainer.classList.remove("invisible");
+}
+
+
 const gameBoard = (() =>{
     let board = ["","","","","","","","",""];
     let sqr = document.getElementsByClassName("sqr");
     let boardContainer = document.getElementById("boardContainer");
+    
     
     //Function that reads the values from the board array and renders the squares
     const renderBoard = () => {
@@ -17,28 +57,32 @@ const gameBoard = (() =>{
     }
 
     //Event Listener
-    const setMark = () =>{
+    const setMark = () => {
         boardContainer.addEventListener("click", function(e){
-            let position = e.target.id;
-            let value = e.target.textContent;
-            if(value === "" && activePlayer === 1){
-                board[position] = "X";  
-                activePlayer = 2;        
-            }else if(value === "" && activePlayer === 2){
-                board[position] = "O";
-                activePlayer = 1;
-            };
-        renderBoard();
-        checkWinner();
-        });
+            if(!onHold === true){
+                let position = e.target.id;
+                let value = e.target.textContent;
+                if(value === "" && activePlayer === 1){
+                    board[position] = "X";  
+                    activePlayer = 2;        
+                }else if(value === "" && activePlayer === 2){
+                    board[position] = "O";
+                    activePlayer = 1;
+                };
+            renderBoard();
+            checkWinner();
+            }
+        })
+        
         
     };
+    
 
     //Clear board function
     const clearBoard = () => {
         board = ["","","","","","","","",""];
         renderBoard();
-    }
+    };
 
     //Check Winner function
 
@@ -73,16 +117,62 @@ const gameBoard = (() =>{
             winner = board[0];
         }else if((cB[2]*cB[4]*cB[6] === 1) || (cB[2]*cB[4]*cB[6] === 8)){
             winner = board[2];    
+        }else if(cB[0] != "" && cB[0] != "" && cB[1] != "" && cB[2] != "" && 
+                 cB[3] != "" && cB[4] != "" && cB[5] != "" && cB[6] != "" && cB[7] != "" && cB[8] != ""){
+            winner = "Tie";
+            messageBoard.textContent ="It's a tie!";
         }
-        console.log(winner);
+        
+        if(winner != ""){
+            onHold = true;
+            scoreWinner(winner);
+        }
 
+    }
+
+    const scoreWinner = (winner) => {
+        if(winner === "X"){
+            playerOne.score++;
+            activePlayer = 2;
+            messageBoard.textContent = playerOne.name + " wins!";
+        }else if(winner === "O"){
+            playerTwo.score++;
+            activePlayer = 1;
+            messageBoard.textContent = playerTwo.name + " wins!";
+        }
+        updateScoreBoard();
+        nextRound();
+    }
+
+    const updateScoreBoard = () => {
+        let p1ScoreBoard = document.getElementById("p1Score");
+        let p2ScoreBoard = document.getElementById("p2Score");
+
+        p1ScoreBoard.textContent = playerOne.score;
+        p2ScoreBoard.textContent = playerTwo.score;
+    }
+
+    const nextRound = () => {
+        let nextRound = document.getElementById("nextRound");
+        nextRound.classList.remove("invisible");
+        nextRound.addEventListener("click", function(){
+            clearBoard();
+            onHold = false;
+            nextRound.classList.add("invisible");
+            if(activePlayer === 1){
+                messageBoard.textContent = playerOne.name + ", your turn!"
+            }else{
+                messageBoard.textContent = playerTwo.name + ", your turn!" 
+            }
+        })
+       
     }
 
 
 
 
 
-return {renderBoard, setMark, clearBoard, checkWinner};
+return {renderBoard, setMark, clearBoard, checkWinner, onHold, updateScoreBoard};
 })();
 
 
